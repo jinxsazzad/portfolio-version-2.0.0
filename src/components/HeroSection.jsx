6a2d2c053b1453da17833229204d62c242c78eda
jinxsazzade <nooruddinsumon55555@gmail.com>
2023-11-04 23:59:34 +0600

    compleating Hero section and Navigation bar

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,59 @@
+"use client";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className=" text-white mb-4 text-3xl sm:text-5xl lg:text-6xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-blue-500 to-pink-500">
+              Hello, I am{" "}
+            </span>
+            Alex
+            {/* <TypeAnimation
+              sequence={[
+                "Sazzad",
+                1000,
+                "Web Developer",
+                1000,
+                "React Developer",
+                1000,
+              ]}
+              speed={40}
+              repeat={Infinity}
+              deletionSpeed={90}
+            /> */}
+          </h1>
+          <p className=" text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            veniam quo quos tempore, nobis blanditiis consequatur
+          </p>
+          <div>
+            <button className=" px-6 py-3 w-full sm:w-fit rounded-full mr-4 mb-2 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white">
+              Hire me
+            </button>
+            <button className=" px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0 ">
+          <div className="rounded-full bg-[#181818] w-[270px] h-[270px] lg:w-[300px] lg:h-[300px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className=" absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={250}
+              height={250}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import MenuOverlay from "./MenuOverlay";
+import NavLink from "./NavLink";
+
+const navLinks = [
+  {
+    id: 1,
+    title: "About",
+    path: "#about",
+  },
+  {
+    id: 2,
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    id: 3,
+    title: "Contact",
+    path: "#contact",
+  },
+];
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          LOGO
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 text-slate-200 hover:text-white"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 text-slate-200 hover:text-white"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+    </nav>
+  );
+};
+
+export default Navbar;
